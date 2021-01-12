293793bdaf026e67b87b43a49a610e73a29c38f6
Kevin O'Connell <kevinoconnell42@gmail.com>
2021-01-11 23:43:08 -0500

    added sidebar and chat functionality to home page

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -2,40 +2,20 @@ import React from "react";
 import { Redirect, useLocation } from 'react-router-dom'
 import PillButton from '../PillButton/PillButton'
 import "./Home.css";
-
+import SideBar from '../SideBar/SideBar'
+import ChatRoom from "../ChatRoom/ChatRoom";
 const Home = () => {
-  const [roomName, setRoomName] = React.useState("");
-
-  const handleRoomNameChange = (event) => {
-    setRoomName(event.target.value);
-  };
-
-   function getToRoom() {
-    return (
-      <PillButton
-        linkTo={`/${roomName}`}
-        name='Go to Room'
-        extraClassName='login'
-        id='Login-submit'
-      />
-    )
-  }
-
-   function handleRoomClick() {
-    const to = { pathname: '/100', state: { from: '/' } }
-    return <Redirect to={to} />
-  }
 
   return (
-    <div className="home-container">
-      <input
-        type="text"
-        placeholder="Room"
-        value={roomName}
-        onChange={handleRoomNameChange}
-        className="text-input-field"
-      />
-      {getToRoom(handleRoomClick)}
+    <div>
+    <SideBar width={250} height={"100vh"}>
+       <h1>Nav Item</h1>
+          <h1>Nav Item</h1>
+          <h1>Nav Item</h1>
+          <h1>Nav Item</h1>
+          <h1>Nav Item</h1>
+    </SideBar>
+    <ChatRoom/>
     </div>
   );
 };
---
+++ frontend/src/Components/SideBar/SideBar.js
@@ -0,0 +1,41 @@
+import "./SideBar.css";
+import React from "react";
+
+export const SideBar = ({ width, height, children }) => {
+  const [xPosition, setX] = React.useState(-width);
+
+  const toggleMenu = () => {
+    if (xPosition < 0) {
+      setX(0);
+    } else {
+      setX(-width);
+    }
+  };
+
+  React.useEffect(() => {
+    setX(0);
+  }, []);
+  return (
+    <React.Fragment>
+      <div
+        className="side-bar"
+        style={{
+          transform: `translatex(${xPosition}px)`,
+          width: width,
+          minHeight: height
+        }}
+      >
+        <button
+          onClick={() => toggleMenu()}
+          className="toggle-menu"
+          style={{
+            transform: `translate(${width}px, 20vh)`
+          }}
+        ></button>
+        <div className="content">{children}</div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default SideBar;
