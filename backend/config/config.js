6d35be6109aaaef7ea1eab63aa4e721cd2824b8d
Kevin O'Connell <kevinoconnell42@gmail.com>
2021-03-19 04:06:23 -0500

    fixed config for database

---
+++ backend/config/config.js
@@ -0,0 +1,28 @@
+require('dotenv').config(); // this is important!
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+  },
+   production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    dialectOptions: { ssl: true },
+    seederStorage: 'sequelize',
+  },
+}
